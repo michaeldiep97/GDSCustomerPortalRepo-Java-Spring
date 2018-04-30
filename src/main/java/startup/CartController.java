cbb150c098923e9412c0d09def76a70ac7c64555
Richard Azille <richieazille@gmail.com>
2018-04-29 23:19:51 -0500

    Added shop controller and view

--- src/main/java/startup/CartController.java
+++ src/main/java/startup/CartController.java
@@ -1,8 +1,12 @@
 package startup;
 
 import java.security.Principal;
+
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class CartController {
@@ -13,4 +17,17 @@ public class CartController {
         }
         return "cart";
     }
+	
+//	@GetMapping("/")
+//	public void testPrint() {
+//		System.out.println("Home clicked from cart");
+//	}
+
+//    if (request.getParameter("Continue Shopping") != null) {
+//        System.out.println("Cart :: 'Continue Shopping' button clicked");
+//    } else if (request.getParameter("Edit Cart") != null) {
+//        System.out.println("Cart :: 'Edit Cart' button clicked");
+//    } else if (request.getParameter("Checkout") != null) {
+//        System.out.println("Cart :: 'Checkout' button clicked");
+//    }
 }

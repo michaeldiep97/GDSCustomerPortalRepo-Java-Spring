17ea4fd268c4e65ceeb9a971b772667351a8b273
Michael Diep <michaeldiep97@gmail.com>
2018-05-02 00:31:47 -0500

    Just kidding, I broke it. This fixes it though. If you uncomment the ProductRepoController, it will break because the product repo bean is somehow not being init'ed through spring.

--- src/main/java/model/ProductRepository.java
+++ src/main/java/model/ProductRepository.java
@@ -4,7 +4,7 @@ import org.springframework.data.repository.CrudRepository;
 
 import model.Product;
 
-// This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
+// This will be AUTO IMPLEMENTED by Spring into a Bean called productRepository
 // CRUD refers Create, Read, Update, Delete
 
 public interface ProductRepository extends CrudRepository<Product, Long> {

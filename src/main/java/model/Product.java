02e9a0b2feecd47be76fef9342406e36918b022e
michaeldiep97 <michaeldiep97@gmail.com>
2018-05-02 22:02:10 -0500

    Update Product.java

--- src/main/java/model/Product.java
+++ src/main/java/model/Product.java
@@ -10,21 +10,21 @@ public class Product {
 	@Id
 	@GeneratedValue(strategy=GenerationType.AUTO)
     private String name;
-    private double price;
-    private int productID;
-    private int category;
+    private Double price;
+    private Integer productID;
+    private Integer category;
 
     // No constructor needed for @Entity classes. 
 
     public void setName(String newName) {
-        name = newName;
+        this.name = newName;
     }
     
     public void setPrice(double newPrice) {
         if (price < 0.00) {
             //throw new CustomException(“Invalid product price assignment”);
         }
-        price = newPrice;
+        this.price = newPrice;
     }
 
     public void setProductID(int newID) {
@@ -32,13 +32,13 @@ public class Product {
     }
     
     public void setCategory(int newCategory) {
-        category = newCategory;
+        this.category = newCategory;
     }
 
     public String getName() { return name; }
-    public double getPrice() { return price; }
-    public int getProductID() { return productID; }
-    public int getCategory() { return category; }
+    public Double getPrice() { return price; }
+    public Integer getProductID() { return productID; }
+    public Integer getCategory() { return category; }
 
     
     // Constructor with category provided    
